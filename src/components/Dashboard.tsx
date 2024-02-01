b9b0d3bf7f1000a52a5877763d46160ccb9ed45f
Ian Percy <ianpercyor@gmail.com>
2024-02-01 06:23:47 -0500

    Adding login form

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import axios from 'axios'
 import { User } from '../models/User'
 import Dwelling from './Dwelling'
@@ -9,22 +9,38 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Alert, FormControl, Modal, OutlinedInput, TextField } from '@mui/material';
 
 function Dashboard() {
     const [users, setUsers] = useState([])
     const [dwellings, setDwellings] = useState([])
     const [user, setUser] = useState<User>()
-    useEffect(() => {
-        axios.get("http://localhost:8080/auth")
-            .then(response => {
-                const newUser = new User(response.data.id, response.data.username, response.data.email)
-                setUser(newUser)
-                console.log(newUser)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }, [])
+    const [loginModalOpen, setLoginModalOpen] = useState(false)
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
+    const style = {
+        position: 'absolute' as 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '1px solid #000',
+        boxShadow: 5,
+        p: 4
+    }
+    // useEffect(() => {
+    //     axios.get("http://localhost:8080/auth")
+    //         .then(response => {
+    //             const newUser = new User(response.data.id, response.data.username, response.data.email)
+    //             // setUser(newUser)
+    //             // console.log(newUser)
+    //         })
+    //         .catch(error => {
+    //             console.log(error)
+    //         })
+    // }, [])
 
     function getDwellings() {
         axios.get("http://localhost:8080/dwelling")
@@ -35,6 +51,29 @@ function Dashboard() {
                 console.log(error)
             })
     }
+    function handleLoginOpen() {
+        setLoginModalOpen(true)
+    }
+    function handleLoginClose() {
+        setLoginModalOpen(false)
+    }
+    function handleSubmit(){
+        axios.post("http://localhost:8080/auth", {username: "jstanley", password: ""}, {headers: {
+            'Content-Type': 'text/json'
+        }})
+        .then(response => {
+            const newUser = new User(response.data.id, response.data.username, response.data.email)
+            setUser(newUser)
+            setLoginModalOpen(false)
+        })
+        .catch(error => {
+            console.log(error)
+            setErrorMessage("Login Unsuccessful. Please try again.")
+            setTimeout(() => setErrorMessage(""), 5000);
+            setLoginModalOpen(false)
+        })
+    }
+
     return (
         <>
             <Box sx={{ flexGrow: 1 }}>
@@ -52,13 +91,20 @@ function Dashboard() {
                         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                             Home Saver
                         </Typography>
-                        {!user ? <Button color="inherit">Login</Button> :
+                        {!user ?
+                            <Button color="inherit" onClick={handleLoginOpen}>Login</Button>
+                            :
                             <Typography variant="h6" component="div">
                                 Welcome, {user.getUsername()}
                             </Typography>
                         }
                     </Toolbar>
                 </AppBar>
+                {errorMessage? 
+                    <Alert severity="error"> {errorMessage}</Alert>
+                    :<></>
+
+                }
                 {/* <div>
                 <span>
                     {users.length !== 0?
@@ -70,6 +116,42 @@ function Dashboard() {
                         : <button onClick={getDwellings}>Get Dwellings</button>}
                 </span>
             </div> */}
+                <Modal
+                    open={loginModalOpen}
+                    onClose={handleLoginClose}
+                >
+                    <Box sx={style}>
+                        <FormControl
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'center'
+                            }}
+                        >
+                            <TextField
+                                id="username"
+                                label="Username"
+                                placeholder="Username"
+                                value={username}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => { setUsername(event.target.value)}}
+                                InputLabelProps={{
+                                    style: { color: "white" }
+                                }}
+                            />
+                            <TextField
+                                id="password"
+                                label="Password"
+                                placeholder="Password"
+                                type="password"
+                                value={password}
+                                onChange={(event: React.ChangeEvent<HTMLInputElement>) => { setPassword(event.target.value)}}
+                                InputLabelProps={{
+                                    style: { color: "white" }
+                                }}
+                            />
+                            <Button variant="outlined" type="submit" onClick={handleSubmit}>Submit</Button>
+                        </FormControl>
+                    </Box>
+                </Modal>
             </Box>
 
         </>
